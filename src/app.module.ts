ee6e9a9b04ce2175956b9b95ed07b0a9b3d347b8
Corina Titica <corina.alexandra.titica@ibm.com>
2019-11-19 12:41:42 +0200

    setup toDo module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {MongooseModule} from '@nestjs/mongoose';
+import { ToDoModule } from './to-do/to-do.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://admin:170096@cluster0-ulutz.mongodb.net/mixin?retryWrites=true&w=majority')],
+  imports: [MongooseModule.forRoot('mongodb+srv://admin:170096@cluster0-ulutz.mongodb.net/mixin?retryWrites=true&w=majority'), ToDoModule],
   controllers: [AppController],
   providers: [AppService],
 })
