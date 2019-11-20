d3276d6b178ee5cb87f9b9159ca20aad865d264a
Irina Rosu <irina.rosu@ro.ibm.com>
2019-11-20 16:18:34 +0200

    post toDo item

--- src/to-do/to-do.controller.ts
+++ src/to-do/to-do.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { ToDoService } from './to-do.service';
+import { CreateToDoDto } from './dtos/create-toDo.dto';
 
 @Controller('to-do')
-export class ToDoController {}
+export class ToDoController {
+  constructor(private readonly toDoService: ToDoService) {}
+
+  @Post()
+  create(@Body() createToDoDto: CreateToDoDto) {
+    this.toDoService.create(createToDoDto);
+  }
+}
--- src/to-do/to-do.service.ts
+++ src/to-do/to-do.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectModel } from '@nestjs/mongoose';
+import { ToDo } from './interfaces/toDo';
+import { Model } from 'mongoose';
+import { CreateToDoDto } from './dtos/create-toDo.dto';
 @Injectable()
-export class ToDoService {}
+export class ToDoService {
+    constructor(@InjectModel('ToDo') private readonly toDoModel: Model<ToDo>) {}
+
+    async create(createToDoDto: CreateToDoDto): Promise<string> {
+        const createDtoModel = this.toDoModel(createToDoDto);
+        const result = await createDtoModel.save();
+        return result.id;
+    }
+}
